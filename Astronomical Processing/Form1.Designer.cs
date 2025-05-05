b20eb51de8d2ca98922e0d4471961abd93c05c4e
Mahay Win <win.mahay@gmail.com>
2025-05-05 23:33:27 +0800

    Initial commit for Astronomical Processing project

--- Astronomical Processing/Form1.Designer.cs
+++ Astronomical Processing/Form1.Designer.cs
@@ -28,31 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
+            lstNeutrinoData = new ListBox();
+            txtSearch = new TextBox();
+            btnSearch = new Button();
+            btnSort = new Button();
+            txtEdit = new TextBox();
+            btnApplyEdit = new Button();
+            lblStatusMessage = new Label();
             SuspendLayout();
             // 
-            // listBox1
+            // lstNeutrinoData
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 25;
-            listBox1.Location = new Point(95, 38);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(180, 129);
-            listBox1.TabIndex = 0;
+            lstNeutrinoData.FormattingEnabled = true;
+            lstNeutrinoData.ItemHeight = 25;
+            lstNeutrinoData.Location = new Point(24, 37);
+            lstNeutrinoData.Name = "lstNeutrinoData";
+            lstNeutrinoData.Size = new Size(166, 604);
+            lstNeutrinoData.TabIndex = 0;
+            lstNeutrinoData.SelectedIndexChanged += lstNeutrinoData_SelectedIndexChanged;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(231, 106);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(231, 31);
+            txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(495, 104);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(112, 34);
+            btnSearch.TabIndex = 2;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click_1;
+            // 
+            // btnSort
+            // 
+            btnSort.Location = new Point(654, 103);
+            btnSort.Name = "btnSort";
+            btnSort.Size = new Size(112, 34);
+            btnSort.TabIndex = 3;
+            btnSort.Text = "Sort";
+            btnSort.UseVisualStyleBackColor = true;
+            btnSort.Click += btnSort_Click_1;
+            // 
+            // txtEdit
+            // 
+            txtEdit.Location = new Point(231, 177);
+            txtEdit.Name = "txtEdit";
+            txtEdit.Size = new Size(231, 31);
+            txtEdit.TabIndex = 4;
+            // 
+            // btnApplyEdit
+            // 
+            btnApplyEdit.Location = new Point(495, 174);
+            btnApplyEdit.Name = "btnApplyEdit";
+            btnApplyEdit.Size = new Size(112, 34);
+            btnApplyEdit.TabIndex = 5;
+            btnApplyEdit.Text = "Apply Edit";
+            btnApplyEdit.UseVisualStyleBackColor = true;
+            btnApplyEdit.Click += btnApplyEdit_Click_1;
+            // 
+            // lblStatusMessage
+            // 
+            lblStatusMessage.AutoSize = true;
+            lblStatusMessage.Location = new Point(231, 48);
+            lblStatusMessage.Name = "lblStatusMessage";
+            lblStatusMessage.Size = new Size(135, 25);
+            lblStatusMessage.TabIndex = 6;
+            lblStatusMessage.Text = "Status Message";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(listBox1);
+            ClientSize = new Size(800, 695);
+            Controls.Add(lblStatusMessage);
+            Controls.Add(btnApplyEdit);
+            Controls.Add(txtEdit);
+            Controls.Add(btnSort);
+            Controls.Add(btnSearch);
+            Controls.Add(txtSearch);
+            Controls.Add(lstNeutrinoData);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Astronomical Processing";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private ListBox listBox1;
+
+        private ListBox lstNeutrinoData;
+        private TextBox txtSearch;
+        private Button btnSearch;
+        private Button btnSort;
+        private TextBox txtEdit;
+        private Button btnApplyEdit;
+        private Label lblStatusMessage;
     }
 }
--- Astronomical Processing/Form1.cs
+++ Astronomical Processing/Form1.cs
@@ -1,10 +1,172 @@
+// Mahay Thi Win, Sprint One
+// Date: 30/04/2025
+// Version: 1
+// Astronomical Processing – Neutrino Data Handler
+// This Windows Forms application processes 24 hourly neutrino readings. 
+// Users can view, sort, search, and edit these values.
+// 
+// Inputs: Randomly generated neutrino readings (10–90), user inputs for search and edit
+// Processes: Populate array, display values in ListBox, sort values using Bubble Sort,
+//            search using Binary Search, allow selection and editing of individual values
+// Outputs: Updated data displayed in ListBox and status messages shown to the user
+
+using System;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace Astronomical_Processing
 {
     public partial class Form1 : Form
     {
+        // Declare array to hold 24 hourly neutrino readings
+        int[] neutrinoData = new int[24];
+
+        // Random number generator to simulate data stream
+        Random rnd = new Random();
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();// Standard WinForms setup
+            InitializeData();// Fill the array with random values
+        }
+
+        // Method to fill the array with random integers between 10 and 90
+        void InitializeData()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                neutrinoData[i] = rnd.Next(10, 91); // 10 to 90
+            }
+            DisplayData(); // Show the data in the ListBox
+        }
+
+        // Method to display all values from the array into the ListBox
+        void DisplayData()
+        {
+            lstNeutrinoData.Items.Clear();
+
+
+            foreach (int value in neutrinoData)
+            {
+                lstNeutrinoData.Items.Add(value);
+            }
+        }
+
+        // Event triggered when the "Sort" button is clicked
+        private void btnSort_Click_1(object sender, EventArgs e)
+        {
+            // Bubble Sort algorithm to sort the array in ascending order
+            for (int i = 0; i < neutrinoData.Length - 1; i++)
+            {
+                for (int j = 0; j < neutrinoData.Length - i - 1; j++)
+                {
+                    if (neutrinoData[j] > neutrinoData[j + 1])
+                    {
+                        // Swap the values
+                        int temp = neutrinoData[j];
+                        neutrinoData[j] = neutrinoData[j + 1];
+                        neutrinoData[j + 1] = temp;
+                    }
+                }
+            }
+
+            lblStatusMessage.Text = "Data sorted."; // Show message
+            DisplayData(); // Refresh the ListBox
+
+        }
+
+        // Event triggered when the "Search" button is clicked
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+
+            // Validate input
+            if (txtSearch.Text == "")
+            {
+                lblStatusMessage.Text = "Search box is empty!";
+                return;
+            }
+
+            int searchValue;
+
+            // Try converting the input to an integer
+            if (!int.TryParse(txtSearch.Text, out searchValue))
+            {
+                lblStatusMessage.Text = "Invalid input!";
+                return;
+            }
+
+            // Perform binary search
+            bool result = BinarySearch(neutrinoData, searchValue);
+            if (!result)
+            {
+                lblStatusMessage.Text = "Value not found.";
+                lstNeutrinoData.ClearSelected();
+            }
+            else
+            {
+                lblStatusMessage.Text = $"Value found.";
+                lstNeutrinoData.SelectedIndex = Array.IndexOf(neutrinoData, searchValue);
+            }
+        }
+
+        // Binary Search method - works only on sorted arrays
+        bool BinarySearch(int[] data, int target)
+        {
+            var sortedData = data.OrderBy(x => x).ToList();
+            int low = 0;
+            int high = sortedData.Count - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (sortedData[mid] == target)
+                    return true; // Found it
+                else if (sortedData[mid] < target)
+                    low = mid + 1; // Search right half
+                else
+                    high = mid - 1;// Search left half
+            }
+
+            return false; // Not found
+
+        }
+
+        // Event triggered when the "Apply Edit" button is clicked
+        private void btnApplyEdit_Click_1(object sender, EventArgs e)
+        {
+            // Check if user selected an item from the ListBox
+            if (lstNeutrinoData.SelectedIndex == -1)
+            {
+                lblStatusMessage.Text = "Please select a value to edit.";
+                return;
+            }
+
+            int newValue;
+            // Validate new value input
+            if (!int.TryParse(txtEdit.Text, out newValue))
+            {
+                lblStatusMessage.Text = "Invalid input!";
+                return;
+            }
+
+            // Update the selected index in the array
+
+            neutrinoData[lstNeutrinoData.SelectedIndex] = newValue;
+            DisplayData(); // Refresh the ListBox
+            lstNeutrinoData.SelectedIndex = Array.IndexOf(neutrinoData, newValue);
+            lblStatusMessage.Text = "Value updated."; // Show success message
+        }
+
+        private void lstNeutrinoData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstNeutrinoData.SelectedIndex == -1)
+            {
+                txtEdit.Text = "";
+            }
+            else
+            {
+                txtEdit.Text = neutrinoData.GetValue(lstNeutrinoData.SelectedIndex)?.ToString();
+            }
         }
     }
 }
