cc8b0d65a6ce2ce9c0e3d3fe4cb6860908a112ac
Mahay-W <win.mahay@gmail.com>
2025-05-15 11:34:26 +0800

    GUI done

--- Astronomical Processing/Form1.Designer.cs
+++ Astronomical Processing/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             lstNeutrinoData = new ListBox();
             txtSearch = new TextBox();
             btnSearch = new Button();
@@ -35,76 +36,196 @@
             txtEdit = new TextBox();
             btnApplyEdit = new Button();
             lblStatusMessage = new Label();
+            txtMidExtreme = new TextBox();
+            btnMidExtreme = new Button();
+            txtMode = new TextBox();
+            btnMode = new Button();
+            txtAverage = new TextBox();
+            btnAverage = new Button();
+            txtRange = new TextBox();
+            btnRange = new Button();
+            toolTip1 = new ToolTip(components);
+            btnSequentialSearch = new Button();
+            txtSequentialSearch = new TextBox();
             SuspendLayout();
             // 
             // lstNeutrinoData
             // 
             lstNeutrinoData.FormattingEnabled = true;
-            lstNeutrinoData.ItemHeight = 25;
-            lstNeutrinoData.Location = new Point(24, 37);
+            lstNeutrinoData.ItemHeight = 15;
+            lstNeutrinoData.Location = new Point(17, 22);
+            lstNeutrinoData.Margin = new Padding(2);
             lstNeutrinoData.Name = "lstNeutrinoData";
-            lstNeutrinoData.Size = new Size(166, 604);
+            lstNeutrinoData.Size = new Size(117, 364);
             lstNeutrinoData.TabIndex = 0;
             lstNeutrinoData.SelectedIndexChanged += lstNeutrinoData_SelectedIndexChanged;
             // 
             // txtSearch
             // 
-            txtSearch.Location = new Point(231, 106);
+            txtSearch.Location = new Point(162, 64);
+            txtSearch.Margin = new Padding(2);
             txtSearch.Name = "txtSearch";
-            txtSearch.Size = new Size(231, 31);
+            txtSearch.Size = new Size(163, 23);
             txtSearch.TabIndex = 1;
             // 
             // btnSearch
             // 
-            btnSearch.Location = new Point(495, 104);
+            btnSearch.Location = new Point(346, 62);
+            btnSearch.Margin = new Padding(2);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(112, 34);
+            btnSearch.Size = new Size(108, 20);
             btnSearch.TabIndex = 2;
-            btnSearch.Text = "Search";
+            btnSearch.Text = "Binary Search";
+            toolTip1.SetToolTip(btnSearch, "Click to search using binary search");
             btnSearch.UseVisualStyleBackColor = true;
             btnSearch.Click += btnSearch_Click_1;
             // 
             // btnSort
             // 
-            btnSort.Location = new Point(654, 103);
+            btnSort.Location = new Point(471, 62);
+            btnSort.Margin = new Padding(2);
             btnSort.Name = "btnSort";
-            btnSort.Size = new Size(112, 34);
+            btnSort.Size = new Size(78, 20);
             btnSort.TabIndex = 3;
             btnSort.Text = "Sort";
+            toolTip1.SetToolTip(btnSort, "Click to sort the data using Bubble Sort");
             btnSort.UseVisualStyleBackColor = true;
             btnSort.Click += btnSort_Click_1;
             // 
             // txtEdit
             // 
-            txtEdit.Location = new Point(231, 177);
+            txtEdit.Location = new Point(162, 106);
+            txtEdit.Margin = new Padding(2);
             txtEdit.Name = "txtEdit";
-            txtEdit.Size = new Size(231, 31);
+            txtEdit.Size = new Size(163, 23);
             txtEdit.TabIndex = 4;
             // 
             // btnApplyEdit
             // 
-            btnApplyEdit.Location = new Point(495, 174);
+            btnApplyEdit.Location = new Point(346, 104);
+            btnApplyEdit.Margin = new Padding(2);
             btnApplyEdit.Name = "btnApplyEdit";
-            btnApplyEdit.Size = new Size(112, 34);
+            btnApplyEdit.Size = new Size(78, 20);
             btnApplyEdit.TabIndex = 5;
             btnApplyEdit.Text = "Apply Edit";
+            toolTip1.SetToolTip(btnApplyEdit, "Click to apply new values");
             btnApplyEdit.UseVisualStyleBackColor = true;
             btnApplyEdit.Click += btnApplyEdit_Click_1;
             // 
             // lblStatusMessage
             // 
             lblStatusMessage.AutoSize = true;
-            lblStatusMessage.Location = new Point(231, 48);
+            lblStatusMessage.Location = new Point(162, 29);
+            lblStatusMessage.Margin = new Padding(2, 0, 2, 0);
             lblStatusMessage.Name = "lblStatusMessage";
-            lblStatusMessage.Size = new Size(135, 25);
+            lblStatusMessage.Size = new Size(88, 15);
             lblStatusMessage.TabIndex = 6;
             lblStatusMessage.Text = "Status Message";
             // 
+            // txtMidExtreme
+            // 
+            txtMidExtreme.Location = new Point(162, 146);
+            txtMidExtreme.Name = "txtMidExtreme";
+            txtMidExtreme.ReadOnly = true;
+            txtMidExtreme.Size = new Size(163, 23);
+            txtMidExtreme.TabIndex = 7;
+            // 
+            // btnMidExtreme
+            // 
+            btnMidExtreme.Location = new Point(346, 145);
+            btnMidExtreme.Name = "btnMidExtreme";
+            btnMidExtreme.Size = new Size(88, 23);
+            btnMidExtreme.TabIndex = 8;
+            btnMidExtreme.Text = "MidExtreme";
+            toolTip1.SetToolTip(btnMidExtreme, "Calculate and display MidExtreme");
+            btnMidExtreme.UseVisualStyleBackColor = true;
+            // 
+            // txtMode
+            // 
+            txtMode.Location = new Point(162, 193);
+            txtMode.Name = "txtMode";
+            txtMode.ReadOnly = true;
+            txtMode.Size = new Size(163, 23);
+            txtMode.TabIndex = 9;
+            // 
+            // btnMode
+            // 
+            btnMode.Location = new Point(346, 193);
+            btnMode.Name = "btnMode";
+            btnMode.Size = new Size(88, 23);
+            btnMode.TabIndex = 10;
+            btnMode.Text = "Mode";
+            toolTip1.SetToolTip(btnMode, "Calculate and display the mode (most frequent)");
+            btnMode.UseVisualStyleBackColor = true;
+            // 
+            // txtAverage
+            // 
+            txtAverage.Location = new Point(162, 239);
+            txtAverage.Name = "txtAverage";
+            txtAverage.ReadOnly = true;
+            txtAverage.Size = new Size(163, 23);
+            txtAverage.TabIndex = 11;
+            // 
+            // btnAverage
+            // 
+            btnAverage.Location = new Point(346, 239);
+            btnAverage.Name = "btnAverage";
+            btnAverage.Size = new Size(88, 23);
+            btnAverage.TabIndex = 12;
+            btnAverage.Text = "Average";
+            toolTip1.SetToolTip(btnAverage, "Calculate the average value");
+            btnAverage.UseVisualStyleBackColor = true;
+            // 
+            // txtRange
+            // 
+            txtRange.Location = new Point(162, 294);
+            txtRange.Name = "txtRange";
+            txtRange.ReadOnly = true;
+            txtRange.Size = new Size(163, 23);
+            txtRange.TabIndex = 13;
+            // 
+            // btnRange
+            // 
+            btnRange.Location = new Point(346, 294);
+            btnRange.Name = "btnRange";
+            btnRange.Size = new Size(88, 23);
+            btnRange.TabIndex = 14;
+            btnRange.Text = "Range";
+            toolTip1.SetToolTip(btnRange, "Calculate and display the range(max-min)");
+            btnRange.UseVisualStyleBackColor = true;
+            // 
+            // btnSequentialSearch
+            // 
+            btnSequentialSearch.Location = new Point(346, 342);
+            btnSequentialSearch.Name = "btnSequentialSearch";
+            btnSequentialSearch.Size = new Size(163, 23);
+            btnSequentialSearch.TabIndex = 16;
+            btnSequentialSearch.Text = "Seqeuential Search";
+            toolTip1.SetToolTip(btnSequentialSearch, "Click to display sequential search");
+            btnSequentialSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSequentialSearch
+            // 
+            txtSequentialSearch.Location = new Point(162, 342);
+            txtSequentialSearch.Name = "txtSequentialSearch";
+            txtSequentialSearch.Size = new Size(163, 23);
+            txtSequentialSearch.TabIndex = 15;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 695);
+            ClientSize = new Size(560, 417);
+            Controls.Add(btnSequentialSearch);
+            Controls.Add(txtSequentialSearch);
+            Controls.Add(btnRange);
+            Controls.Add(txtRange);
+            Controls.Add(btnAverage);
+            Controls.Add(txtAverage);
+            Controls.Add(btnMode);
+            Controls.Add(txtMode);
+            Controls.Add(btnMidExtreme);
+            Controls.Add(txtMidExtreme);
             Controls.Add(lblStatusMessage);
             Controls.Add(btnApplyEdit);
             Controls.Add(txtEdit);
@@ -112,6 +233,7 @@
             Controls.Add(btnSearch);
             Controls.Add(txtSearch);
             Controls.Add(lstNeutrinoData);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "Astronomical Processing";
             ResumeLayout(false);
@@ -128,5 +250,16 @@
         private TextBox txtEdit;
         private Button btnApplyEdit;
         private Label lblStatusMessage;
+        private TextBox txtMidExtreme;
+        private Button btnMidExtreme;
+        private TextBox txtMode;
+        private Button btnMode;
+        private TextBox txtAverage;
+        private Button btnAverage;
+        private TextBox txtRange;
+        private Button btnRange;
+        private ToolTip toolTip1;
+        private TextBox txtSequentialSearch;
+        private Button btnSequentialSearch;
     }
 }
